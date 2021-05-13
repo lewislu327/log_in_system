34e1f99db11e3a7949b7087fe08f288b81bd844e
Lewis Lu <agooday32@gmail.com>
2021-05-13 16:30:44 +0800

    feat: add log in success msg

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser') 
+const White_List = require('./models/white_list')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/white-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -14,39 +15,6 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const users = [
- {
-   firstName: 'Tony',
-   email: 'tony@stark.com',
-   password: 'iamironman'
- },
- {
-   firstName: 'Steve',
-   email: 'captain@hotmail.com',
-   password: 'icandothisallday'
- },
- {
-   firstName: 'Peter',
-   email: 'peter@parker.com',
-   password: 'enajyram'
- },
- {
-   firstName: 'Natasha',
-   email: 'natasha@gamil.com',
-   password: '*parol#@$!'
- },
- {
-   firstName: 'Nick',
-   email: 'nick@shield.com',
-   password: 'password'
- },
- {
-   firstName: '111',
-   email: '111@111',
-   password: '111'
- }
-]
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true}))
@@ -56,19 +24,18 @@ app.get('/', (req,res) =>
 )
 
 app.post('/show', (req, res) => {
-  const email = req.body.mailaddress
-  const password = req.body.password
-  const approvedEmail = users.indexOf(email)
-  const correctPW = users.indexOf(password)
-  const name = 
-
-  if (approvedEmail && correctPW ){
-    console.log('hello')
-    welcome = `Welcomeback, ${users.firstName}`
-  }
-  
-  res.render('show', {welcome})
-  
+  const { email, password } = req.body
+  const errorMsg = '帳號/密碼錯誤'
+  return White_List.findOne({ email: email, password: password })
+  .lean()
+  .then(user => {
+      if (user) {
+        welcomeMSG = `Welcome back, ${user.firstName}`
+        res.render('show',{welcomeMSG} )
+      } else {
+        res.render('show', { errorMsg })
+      }
+  })
 })
 
 

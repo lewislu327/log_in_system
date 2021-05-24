da0f0472239deb5babeabfacf82fb629baf02b26
Lewis Lu <agooday32@gmail.com>
2021-05-24 16:53:51 +0800

    feat: add cookie-parser and express-session to stay login status

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser')
 const White_List = require('./models/white_list')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/white-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const cookieParser = require('cookie-parser')
+const session = require('express-session') 
+
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -18,10 +21,28 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true}))
+app.use(cookieParser('55688'))
+app.use(session({
+  // name: 'userName',
+  secret: '55688',
+  resave: false,                //要查
+  saveUninitialized: false,     //要查
+}))
 
-app.get('/', (req,res) =>
-  res.render('index')
-)
+app.get('/', (req,res) => {
+  const firstName = req.session.userName
+  console.log(firstName)
+  White_List.findOne({firstName})
+  .lean()
+  .then(user => {
+      if (user) { 
+        welcomeMSG = `You've already log-in, ${user.firstName}`
+        res.render('show',{welcomeMSG} )
+      } else {
+        res.render('index')
+      }
+  })
+})
 
 app.post('/show', (req, res) => {
   const { email, password } = req.body
@@ -30,6 +51,11 @@ app.post('/show', (req, res) => {
   .lean()
   .then(user => {
       if (user) {
+        // res.cookie('userName', user.firstName, { signed: true, maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+        console.log(user)
+        req.session.userName = user.firstName
+        console.log(user.firstName)
+        // console.log(req.session)
         welcomeMSG = `Welcome back, ${user.firstName}`
         res.render('show',{welcomeMSG} )
       } else {
@@ -38,6 +64,10 @@ app.post('/show', (req, res) => {
   })
 })
 
+app.get('/logout', (req, res) => {
+  res.clearCookie('connect.sid', { path: '/' })
+  return res.redirect('/')
+})
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
